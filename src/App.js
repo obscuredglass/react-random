69f0eaeedbd43655cb525050b3e763571a3569dd
ahochman <ahochman2019@gmail.com>
2019-05-28 18:22:20 -0400

    adding navbar

--- src/App.js
+++ src/App.js
@@ -5,83 +5,96 @@ import friends from "./friends.json";
 import "./App.css";
 
 
-class App extends React.Component{
+class App extends React.Component {
   // we made it a class so we can keep track of 'state'
 
   state = {
-    friendList: friends,
-    clickedFriends,
-    score
-  };
-
-setClicked = event => {
-const currentFriends = this.state.alt;
-const FriendsAlreadyClicked =
-this.state.clickedFriends.indexOf(currentFriends) > -1;
-
-if (FriendsAlreadyClicked) {
-  this.setState({
-    friends: this.state.friendList.sort(function(a, b) {
-      return 0.5 - Math.random();
-    }),
+    friends,
     clickedFriends: [],
     score: 0
-  });
-  alert("You have lost the game. Dare you play again?")
-}
-
-else  {
-  this.setState(
-    {
-      friendList: this.state.friendList.sort(function(a, b){
-        return 0.5 - Math.random();
-      }),
-      clickedFriends: this.state.clickedFriends.concat(
-        currentFriends
-      ),
-      score: this.state.score + 1
-    }
-  )
-}
-
-
-
-}
-
-
-
-
-
-
-  shuffleCards = () => {
-    const shuffledFriendList = this.state.friendList.sort(() => 0.5 - Math.random());
-  
-    this.setState({
-      friendList: shuffledFriendList
-    });
   };
 
-render () {
-  return (
-    <Wrapper>
-      <h1 className="title">Friends List</h1>
-    {
-      this.state.friendList.map(friend =>
-        <FriendCard
-        key={friend.id}
-        id={friend.id}
-        name={friend.name}
-        image={friend.image}
-        occupation={friend.occupation}
-        location={friend.location}
-        shuffleCards={this.shuffleCards}
-        />
-        )
-    }
+  setClicked = event => {
+    const currentFriends = this.state.alt;
+    const FriendsAlreadyClicked =
+      this.state.clickedFriends.indexOf(currentFriends) > -1;
 
-    </Wrapper>
-  );
-}
+    if (FriendsAlreadyClicked) {
+      this.setState({
+        friends: this.state.friends.sort(function (a, b) {
+          return 0.5 - Math.random();
+        }),
+        clickedFriends: [],
+        score: 0
+      });
+      alert("You have lost the game. Dare you play again?")
+    }
+    // if you click on a friend who hasn't been clicked your score goes up and your cards are shuffled
+    else {
+      this.setState(
+        {
+          friends: this.state.friends.sort(function (a, b) {
+            return 0.5 - Math.random();
+          }),
+          clickedFriends: this.state.clickedFriends.concat(
+            currentFriends
+          ),
+          score: this.state.score + 1
+        },
+        // if you get all 12 friends you win, you get a winning message and the game starts back over again
+        () => {
+          if (this.state.score === 12) {
+            alert("Woohoo! You've Won! Now you can rememeber everything!");
+            this.setState({
+              friends: this.state.friends.sort(function (a, b) {
+                return 0.5 - Math.random();
+              }),
+              clickedFriends: [],
+              score: 0
+            });
+          }
+        }
+      );
+    }
+  };
+
+
+
+
+
+
+  // shuffleCards = () => {
+  //   const shuffledFriendList = this.state.friendList.sort(() => 0.5 - Math.random());
+
+  //   this.setState({
+  //     friendList: shuffledFriendList
+  //   });
+  // };
+
+  render() {
+    return (
+      <Wrapper>
+        <Navbar
+          score={this.state.score}
+        />
+        <h1 className="title">Friends List</h1>
+        {
+          this.state.friends.map(friends =>
+            <FriendCard
+              key={friends.id}
+              id={friends.id}
+              name={friends.name}
+              image={friends.image}
+              occupation={friends.occupation}
+              location={friends.location}
+              // shuffleCards={this.shuffleCards}
+            />
+          )
+        }
+
+      </Wrapper>
+    );
+  }
 }
 
 export default App;
